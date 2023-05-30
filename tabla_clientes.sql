07fa604dc0925f3e41ec5d9ac4d21030aa120bf2
Livni <llmc0383@gmail.com>
2023-05-29 21:15:04 -0600

    se modifico la tabla clientes

--- tabla_clientes.sql
+++ tabla_clientes.sql
@@ -1,7 +1,7 @@
-Create table cientes (
+Create table clientes (
 
-    Cliente_id serial primary key,
-    Cliente_nombre varchar (60) not null,
+    cliente_id serial primary key,
+    cliente_nombre varchar (60) not null,
     cliente_nit varchar (10) not null,
     cliente_situacion char (1) 
 );
