d685bbf3070a5da4664956d8308181daafe50cd6
Livni <llmc0383@gmail.com>
2023-05-30 23:50:15 -0600

    se coloco required

--- vistas/clientes/modificar.php
+++ vistas/clientes/modificar.php
@@ -20,13 +20,13 @@ require '../../modelos/cliente.php';
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cliente_nombre">Nombre del cliente</label>
-                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control">
+                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cliente_nit">Nit del cliente</label>
-                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control">
+                        <input type="text" name="cliente_nit" id="cliente_nit" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
